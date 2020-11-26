ad0ff13fa454db3c57da93081fbb70cfd1c1a6f8
ngapham <nga.pham.cs83@gmail.com>
2020-11-26 15:54:08 +0100

    Experiment with screenshotmachine sample code

---
+++ main.js
@@ -0,0 +1,43 @@
+const screenshotmachine = require("screenshotmachine");
+
+const urlGenerated = () => {
+  const customerKey = "b01b3d";
+  secretPhrase = ""; //leave secret phrase empty, if not needed
+  options = {
+    //mandatory parameter
+    url: "https://api.screenshotmachine.com",
+    // all next parameters are optional, see our website screenshot API guide for more details
+    dimension: "1920x1080", // or "1366xfull" for full length screenshot
+  };
+  return screenshotmachine.generateScreenshotApiUrl(
+    customerKey,
+    secretPhrase,
+    options
+  );
+};
+
+test = async () => {
+  let url = urlGenerated();
+
+  //save screenshot as an image
+  var fs = require("fs");
+  var output = "output.png";
+  screenshotmachine.readScreenshot(url).pipe(
+    fs.createWriteStream(output).on("close", function () {
+      console.log("Screenshot saved as " + output);
+    })
+  );
+
+  // const fetch = require("node-fetch");
+  // let response = await fetch(url);
+
+  // if (response.ok) {
+  //   // if HTTP-status is 200-299
+  //   let json = await response.body;
+  //   // console.log(json);
+  // } else {
+  //   console.log("HTTP-Error: " + response.status);
+  // }
+};
+
+test();
