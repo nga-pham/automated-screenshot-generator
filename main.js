492e62edcc5679419ceb0d93b035c3a9490ad9cb
ngapham <nga.pham.cs83@gmail.com>
2020-11-28 06:43:49 +0100

    read id and url of webpages from json file

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 const screenshotmachine = require("screenshotmachine");
 
-const urlGenerated = () => {
+/**
+ * Function to generate url
+ * @param {url parameter} url
+ */
+const generateUrl = (url) => {
   const customerKey = "b01b3d";
   secretPhrase = ""; //leave secret phrase empty, if not needed
   options = {
     //mandatory parameter
-    url: "https://ifunded.de/en/",
-    // all next parameters are optional, see our website screenshot API guide for more details
+    url,
     dimension: "1920x1080", // or "1366xfull" for full length screenshot
   };
   return screenshotmachine.generateScreenshotApiUrl(
@@ -16,17 +19,29 @@ const urlGenerated = () => {
   );
 };
 
-test = async () => {
-  let url = urlGenerated();
+/**
+ * Function to read data
+ * @param fs file system variable
+ */
+readData = async (fs) => {
+  let path = "./data/webPages.json";
+  const buffer_data = await fs.readFile(path);
+  return JSON.parse(buffer_data.toString());
+};
 
+generator = async () => {
+  let url = "https://ifunded.de/en/";
+  let url_generated = generateUrl(url);
+  var fs = require("fs").promises;
+  let data = await readData(fs);
+  console.log(data);
   //save screenshot as an image
-  var fs = require("fs");
-  var output = "output.png";
-  screenshotmachine.readScreenshot(url).pipe(
-    fs.createWriteStream(output).on("close", function () {
-      console.log("Screenshot saved as " + output);
-    })
-  );
+  // var output = "iFunded_name.jpg";
+  // screenshotmachine.readScreenshot(url_generated).pipe(
+  //   fs.createWriteStream(output).on("close", () => {
+  //     console.log("Screenshot saved as " + output);
+  //   })
+  // );
 
   // const fetch = require("node-fetch");
   // let response = await fetch(url);
@@ -40,4 +55,5 @@ test = async () => {
   // }
 };
 
-test();
+// Run the generator
+generator();
